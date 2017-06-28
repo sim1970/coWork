d6829256c91b6c374c4d0d1ce17f55209e17d401
sim1970 <sim1970@wp.pl>
2017-06-28 13:32:21 +0200

    initial commit coWork RL

---
+++ src/main/java/helloWorld/HWRL2.java
@@ -0,0 +1,10 @@
+package helloWorld;
+
+/**
+ * Created by Toshiba on 2017-06-28.
+ */
+public class HWRL2 {
+    public static void main(String[] args) {
+        System.out.println("coWork");
+    }
+}
